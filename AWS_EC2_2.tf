7087dbd2adefdac062e97b6fb07c7a6ed5b71f72
melbit-adityasinha <mahendraditya.sinha@melbourneit.com.au>
2018-05-25 12:24:08 +1000

    Updated File Resources Section

--- AWS_EC2_2.tf
+++ AWS_EC2_2.tf
@@ -3,5 +3,21 @@ provider "aws" {
       access_key = "${var.AWS_ACCESS_KEY}"
       secret_key = "${var.AWS.SECRET_KEY}"
   }
+resource "aws_instance" "Instance_Creation" {
+  ami = "ami-105546a"
+  instance_type = "m4.xlarge"
+  key_name = "${aws_key_Pair_testkey}"
+  tags {
+    name = "Test_Instance_creation"
+  }
+}
+resource "aws_instance" "Instance_Deletion" {
+  ami = "ami-105546a"
+  instance_type = "t2.medium"
+  key_name = "${aws_key_Pair_testkey}"
+  tags {
+    name = "Test_Instance_creation"
+  }
+}
 
 
